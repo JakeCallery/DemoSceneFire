de41683d9361df694578f7d045381b40bc24665d
Jake <jake@subvoicestudios.com>
2023-10-10 16:07:23 -0500

    minor clean up

--- app/components/FireCanvas/FireCanvas.tsx
+++ app/components/FireCanvas/FireCanvas.tsx
@@ -11,7 +11,6 @@ interface FireCanvasProps {
 const NUM_DATA_COLOR_BYTES = 4;
 const FireCanvas = ({ width, height, palette }: FireCanvasProps) => {
   const fireDataRef = useRef(new Uint8ClampedArray(width * height));
-
   function randomizeFirstRow() {
     const bottomRowOffset = (height - 1) * width;
     for (let xOffset = 0; xOffset < width; xOffset++) {
@@ -21,7 +20,6 @@ const FireCanvas = ({ width, height, palette }: FireCanvasProps) => {
     }
   }
 
-  randomizeFirstRow();
   function updatePixels(imageData: ImageData) {
     //generate first row of random values (will be used as palette indices)
     randomizeFirstRow();
@@ -29,24 +27,27 @@ const FireCanvas = ({ width, height, palette }: FireCanvasProps) => {
     //update fire data values using near by values
     for (let y = 0; y < height - 1; y++) {
       for (let x = 0; x < width; x++) {
+        //Down 1 Left 1
         const val1 =
           fireDataRef.current[
             ((y + 1) % height) * width + ((x - 1 + width) % width)
-          ]; //Down 1 left 1
-        const val2 =
-          fireDataRef.current[((y + 1) % height) * width + (x % width)]; //Down 1 Center
+          ];
+
+        //Down 1 Center
+        const val2 = fireDataRef.current[((y + 1) % height) * width + x];
+
+        //Down 1 Right 1
         const val3 =
-          fireDataRef.current[((y + 1) % height) * width + ((x + 1) % width)]; //Down 1 right 1
-        const val4 =
-          fireDataRef.current[(((y + 2) * width) % height) + (x % width)]; //Down 2 Center
+          fireDataRef.current[((y + 1) % height) * width + ((x + 1) % width)];
+
+        //Down 2 Center
+        const val4 = fireDataRef.current[(((y + 2) * width) % height) + x];
+
         const summedValue = val1 + val2 + val3 + val4;
+
         fireDataRef.current[y * width + x] = Math.floor(
           (summedValue * 600) / 1801,
         );
-        // fireDataRef.current[y * width + x] = Math.floor(
-        //   (summedValue * 15) / 64,
-        // );
-        // fireDataRef.current[y * width + x] = summedValue;
       }
     }
 
